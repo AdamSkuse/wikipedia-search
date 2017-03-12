3be6c015fd3a02f7000d8c7c1e88010bed87bac4
AdamSkuse <a.t.skuse@gmail.com>
2017-03-12 19:31:13 +0000

    Adds API

--- script.js
+++ script.js
@@ -8,5 +8,12 @@ setupEventListeners();
 function submitSearch() {
     var searchString = document.getElementById('search-string').value;
     console.log(searchString);
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=&origin=*&srsearch=" + searchString, function(json) {
+        renderResults(json);
+    });
+}
+
+function renderResults(results) {
+    console.log('RESULTS START HERE', JSON.stringify(results));
 }
 
